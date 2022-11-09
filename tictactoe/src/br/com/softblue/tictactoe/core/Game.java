fff6078eb3473b9c1e0cd42ae97897b543bfd1db
valeskamdev <valeskkitty@gmail.com>
2022-11-09 15:10:54 -0300

    método que retorna o proximo jogador

--- tictactoe/src/br/com/softblue/tictactoe/core/Game.java
+++ tictactoe/src/br/com/softblue/tictactoe/core/Game.java
@@ -7,6 +7,7 @@ public class Game {
 
     private Board board = new Board();
     private Player[] players = new Player[Constantes.SYMBOL_PLAYERS.length];  // definindo o numero de jogadores pela quantidade de simbolos
+    private int currentPlayerIndex = 0; // indice do jogador atual
 
     public void play() {
 
@@ -26,6 +27,24 @@ public class Game {
         Player player = new Player(name, board, symbol );  // criando um novo jogador com o nome, tabuleiro e símbolo
         UI.printText("O jogador " + "'" + name + "'" + " vai usar o símbolo " + "'" + symbol + "'");
         return player;
+    }
+
+    // método que retorna o próximo jogador
+    private Player nextPlayer() {
+
+        currentPlayerIndex++;  // incrementando o índice do player atual(0)
+
+        //quando chega no final da fila, recomeça a fila do 0
+        if(currentPlayerIndex >= players.length) {
+            currentPlayerIndex = 0;
+        }
+
+        return players[currentPlayerIndex];
+
+        /* ---OUTRA OPÇÃO DE ABORDAGEM---
+           aqui ele vai pegar o valor atual do indice e dividir pelo tamanho do array
+           jogadorAtual = (jogadorAtual +1) % players.length;
+           return players[jogadorAtual]; */
 
     }
 }
